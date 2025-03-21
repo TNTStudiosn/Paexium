d6ea973cf8a542635d567e5eeffbfa5f89b7c2cf
TNTStudios <tntstudiosn@gmail.com>
2025-03-21 14:38:17 -0600

    fix aviso mixin y se metio items de votacion

--- src/main/java/com/TNTStudios/paexium/items/PaexiumItems.java
+++ src/main/java/com/TNTStudios/paexium/items/PaexiumItems.java
@@ -1,20 +1,35 @@
 package com.TNTStudios.paexium.items;
 
-import net.minecraft.item.*;
+import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class PaexiumItems {
 
+    public static final Item CASCO = new Item(new Item.Settings().maxCount(1));
 
-    public static final Item CASCO = new ArmorItem(
-            ArmorMaterials.NETHERITE,
-            ArmorItem.Type.HELMET,
-            new Item.Settings().maxCount(1)
-    );
+    public static final Item PALETA_1 = new Item(new Item.Settings());
+    public static final Item PALETA_2 = new Item(new Item.Settings());
+    public static final Item PALETA_3 = new Item(new Item.Settings());
+    public static final Item PALETA_4 = new Item(new Item.Settings());
+    public static final Item PALETA_5 = new Item(new Item.Settings());
+    public static final Item PALETA_6 = new Item(new Item.Settings());
+    public static final Item PALETA_7 = new Item(new Item.Settings());
+    public static final Item PALETA_8 = new Item(new Item.Settings());
+    public static final Item PALETA_9 = new Item(new Item.Settings());
 
     public static void register() {
         Registry.register(Registries.ITEM, new Identifier("paexium", "casco"), CASCO);
+
+        Registry.register(Registries.ITEM, new Identifier("paexium", "paleta1"), PALETA_1);
+        Registry.register(Registries.ITEM, new Identifier("paexium", "paleta2"), PALETA_2);
+        Registry.register(Registries.ITEM, new Identifier("paexium", "paleta3"), PALETA_3);
+        Registry.register(Registries.ITEM, new Identifier("paexium", "paleta4"), PALETA_4);
+        Registry.register(Registries.ITEM, new Identifier("paexium", "paleta5"), PALETA_5);
+        Registry.register(Registries.ITEM, new Identifier("paexium", "paleta6"), PALETA_6);
+        Registry.register(Registries.ITEM, new Identifier("paexium", "paleta7"), PALETA_7);
+        Registry.register(Registries.ITEM, new Identifier("paexium", "paleta8"), PALETA_8);
+        Registry.register(Registries.ITEM, new Identifier("paexium", "paleta9"), PALETA_9);
     }
 }
--- src/main/java/com/TNTStudios/paexium/mixin/ExplosionMixin.java
+++ src/main/java/com/TNTStudios/paexium/mixin/ExplosionMixin.java
@@ -7,8 +7,13 @@ import org.spongepowered.asm.mixin.Overwrite;
 @Mixin(Explosion.class)
 public class ExplosionMixin {
 
+    /**
+     * Evita que cualquier explosión destruya bloques en el mundo.
+     * Esto incluye TNT, Creepers, Ghasts, etc.
+     */
     @Overwrite
     public boolean shouldDestroy() {
         return false; // Nunca destruye bloques
     }
+
 }
