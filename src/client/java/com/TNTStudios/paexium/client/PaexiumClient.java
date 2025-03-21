1614ed0cc7cf80fdb208e5d7b8023499f9829d55
TNTStudios <tntstudiosn@gmail.com>
2025-03-20 22:50:59 -0600

    Initial commit

---
+++ src/client/java/com/TNTStudios/paexium/client/PaexiumClient.java
@@ -0,0 +1,10 @@
+package com.TNTStudios.paexium.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PaexiumClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/TNTStudios/paexium/Paexium.java
@@ -0,0 +1,10 @@
+package com.TNTStudios.paexium;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Paexium implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
